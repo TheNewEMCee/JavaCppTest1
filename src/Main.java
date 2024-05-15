0dbbb950ab4a6c6c94899dcc7e908fd9b7ae9618
Christine Swann <christineswann@Christines-MacBook-Air.local>
2024-05-15 09:48:38 -0400

    to10
    changed 5 to 10

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.println("i = " + i);
         }
     }
