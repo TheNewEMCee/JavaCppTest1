b00da0bd5a43135662ec28449d26bd522b979acf
TheNewEMCee <emilybmariamc@gmail.com>
2024-05-17 11:32:53 -0400

    At

--- cpp/CInt.cpp
+++ cpp/CInt.cpp
@@ -12,8 +12,9 @@ CInt::CInt() {
     this->value = 0;
 }
 
-CInt::CInt(std::string name, int lineToReference, int value, std::ifstream inFile) {
+CInt::CInt(std::string name, std::string path, int lineToReference, int value, std::ifstream inFile) {
     this->name = std::move(name);
+    this->path = std::move(path);
     this->lineToReference = lineToReference;
     this->value = value;
     this->inFile;
@@ -23,9 +24,43 @@ CInt::~CInt() {
     std::cout << "CInt destroyed." << std::endl;
 }
 
-void CInt::update() {
+void CInt::updateAll() {
+    int currentLine = 0;
+    std::string line;
+    this->inFile.open(this->path);
+    while(!inFile.eof()) {
+        currentLine++;
+        getline(inFile, line);
+        if (currentLine == this->lineToReference) {
+            this->value = std::stoi(line);
+            break;
+        }
+    }
+    this->inFile.close();
+}
+
+void CInt::updateValue() {
 
 }
 
+void CInt::setName(std::string n) {
+
+}
+
+void CInt::setPath(std::string p) {
+
+}
+
+void CInt::setLineToReference(int l) {
+
+}
+
+void CInt::setValue(int n) {
+
+}
+
+void CInt::setInFile(std::ifstream infile) {
+
+}
 
 
