6cdf79498967ac37c0e03b78e1000a981874a879
Christine Swann <christineswann@Christines-MacBook-Air.local>
2024-05-17 11:42:27 -0400

    Java reading Text File
    I did another thing :D

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,31 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
-        
+        File file = new File("/Users/christineswann/Documents/GitHub/JavaCppTest1/text/Data1.txt");
 
-            File file = new File("/Users/christineswann/Documents/GitHub/JavaCppTest1/text/Data1.txt");
+//Writning:
+            
             FileWriter fr = new FileWriter(file, true);
-            BufferedWriter br = new BufferedWriter(fr);
-            br.write("Writing from the java file!!!!");
-            br.close();
-           
+            BufferedWriter bw = new BufferedWriter(fr);
+            bw.write("Writing from the java file!!!!");
+            bw.close();
+//Reading:
+        FileInputStream fis = new FileInputStream(file);
+        InputStreamReader isr = new InputStreamReader(fis);
+        BufferedReader br = new BufferedReader(isr);
+
+        String line;
+        while((line = br.readLine()) != null){
+            //process the line
+            System.out.println(line);
+        }
+        br.close();
 
     }
 }
