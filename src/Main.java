59c0a3f042307705092ccc7e96881e5bc7caf906
TheNewEMCee <emilybmariamc@gmail.com>
2024-05-21 10:34:57 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,36 @@ import java.io.FileWriter;
 import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
-        
+        try {
 
-            File file = new File("/Users/christineswann/Documents/GitHub/JavaCppTest1/text/Data1.txt");
-            FileWriter fr = new FileWriter(file, true);
-            BufferedWriter br = new BufferedWriter(fr);
-            br.write("Writing from the java file!!!!");
-            br.close();
-           
+            String filename = "Data1.txt";
+            String workingDirectory = System.getProperty("user.dir");
 
+            //****************//
+
+            String absoluteFilePath = "";
+
+            //absoluteFilePath = workingDirectory + System.getProperty("file.separator") + filename;
+            absoluteFilePath = workingDirectory + File.separator + filename;
+
+            System.out.println("Final filepath : " + absoluteFilePath);
+
+            //****************//
+
+            File file = new File(absoluteFilePath);
+
+            if (file.createNewFile()) {
+                    System.out.println("File is created!");
+                } else {
+                    System.out.println("File is already existed!");
+                }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        FileWriter fr = new FileWriter(file, true);
+        BufferedWriter br = new BufferedWriter(fr);
+        br.write("Writing from the java file!!!!");
+        br.close();
     }
 }
